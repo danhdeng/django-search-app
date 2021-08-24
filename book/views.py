55fbb43fecfb13320a574d3f564a009a704a1c18
Dan Deng <hui_danny_deng@hotmail.com>
2021-08-24 13:16:48 -0400

    update docker compose file, add postgres default db in settings

--- book/views.py
+++ book/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def customer_view(
+    self,
+    argument1,
+    argument2,
+    argument3,
+    argument4,
+    argument5,
+    argument6,
+    argument7,
+    argument8,
+    argument9,
+    argument10,
+    argument11,
+):
+    pass
